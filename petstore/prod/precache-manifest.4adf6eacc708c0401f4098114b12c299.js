ac8ee9c53c3d64a5bd5a78605638c6f3e886b2ae
Renato Picco <56326785+repicco@users.noreply.github.com>
2020-05-19 00:36:03 -0300

    Deploy.Prod

---
+++ petstore/prod/precache-manifest.4adf6eacc708c0401f4098114b12c299.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb403c5f99f5d0f106753f9a2035e8b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f1f56fe249c90174846",
+    "url": "/static/css/main.463c7a7c.chunk.css"
+  },
+  {
+    "revision": "5b0e3e98fff544c70748",
+    "url": "/static/js/2.94923852.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.94923852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1f56fe249c90174846",
+    "url": "/static/js/main.bd0fa937.chunk.js"
+  },
+  {
+    "revision": "8f1cfe874cc6cf837762",
+    "url": "/static/js/runtime-main.37e5c98c.js"
+  },
+  {
+    "revision": "784f6233037a2e91dec65a3c46ce7714",
+    "url": "/static/media/acessorios.784f6233.jpg"
+  },
+  {
+    "revision": "2fc8d60af159d018e1307540d5d918b0",
+    "url": "/static/media/banner1.2fc8d60a.jpg"
+  },
+  {
+    "revision": "6bf1dc6f83a60578606674011661fe72",
+    "url": "/static/media/banner2.6bf1dc6f.jpg"
+  },
+  {
+    "revision": "5648cb19ed5112cbc7ce3ec21e8d35a3",
+    "url": "/static/media/banner3.5648cb19.jpg"
+  },
+  {
+    "revision": "15a57740b6e466bbd0c04a0438fa1645",
+    "url": "/static/media/brinquedos.15a57740.jpg"
+  },
+  {
+    "revision": "7301f817219d6945690f58f63686fd0f",
+    "url": "/static/media/cao.7301f817.jpg"
+  },
+  {
+    "revision": "a37e0ae84531f225b327350adaea3837",
+    "url": "/static/media/exotico.a37e0ae8.jpg"
+  },
+  {
+    "revision": "c0846ab9a91ae17ee3c42e98786b3b14",
+    "url": "/static/media/gato.c0846ab9.jpg"
+  },
+  {
+    "revision": "9cb0db7e74381f9d323f9c58c3cb1ae6",
+    "url": "/static/media/logo.9cb0db7e.png"
+  },
+  {
+    "revision": "50bd99178d696b57758d48064bffec83",
+    "url": "/static/media/pagamentos.50bd9917.jpg"
+  },
+  {
+    "revision": "c413750616a5f0a650458d9dd81841ae",
+    "url": "/static/media/passaros.c4137506.jpg"
+  },
+  {
+    "revision": "e8db55f3d7107b5a3241c2b1c4470e33",
+    "url": "/static/media/prod1.e8db55f3.jpg"
+  },
+  {
+    "revision": "c8ecd6c023ec260dd2c4f19cd0dd00ee",
+    "url": "/static/media/prod4.c8ecd6c0.jpg"
+  },
+  {
+    "revision": "8231e92298bc8d36d8db78369f3276bd",
+    "url": "/static/media/racoes.8231e922.jpg"
+  }
+]);
